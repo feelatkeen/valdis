e527f880b5ef4a7b54ab87e5e4627e1a2973de21
Vlad Filatov <vladfox1337@yandex.ru>
2017-08-16 18:22:53 +0300

    hi

--- valdis.py
+++ valdis.py
@@ -68,10 +68,7 @@ def main():
                     wordlist = wordstring.split()
                     worddict = dict([(wordlist[i], wordlist[i+1]) for i in range(0, len(wordlist) - 1, 2) ])
                     shizalength = random.randint(0,20)
-                    print(wordlist)
-                    print("markov:")
                     sent_model = make_markov_model(wordlist)
-                    print(sent_model)
                     shizasent = generate_random_sentence(shizalength, sent_model)
                     if shizasent == "" or shizasent == " ":
                         vk.messages.send(peer_id=event.peer_id, message="простите пожалуйста, я не знаю что сказать. попробуйте еще раз. извините за доставленные неудобства")
