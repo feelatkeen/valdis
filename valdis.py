b9bf6b3e6b7e5a819fdb5360848dc19945841c5f
Vlad Filatov <vladfox1337@yandex.ru>
2017-08-16 18:32:57 +0300

    hi

--- valdis.py
+++ valdis.py
@@ -37,7 +37,7 @@ def main():
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
-            if event.text == "шизик добавь меня":
+            if event.text.lower() == "шизик добавь меня":
                 friendget = vk.friends.areFriends(user_ids=event.user_id)
                 friendare = friendget[0]
                 friendare = friendare['friend_status']
@@ -48,17 +48,17 @@ def main():
                     vk.friends.add(user_id=event.user_id)
                 else:
                     vk.messages.send(peer_id=event.peer_id, message="кинь заявку потом еще раз попробуй)")
-            if event.text == "шизик ping":
+            if event.text.lower() == "шизик ping":
                 vk.messages.send(peer_id=event.peer_id, message="pong") 
-            elif event.text == "шизик помощь":
+            elif event.text.lower() == "шизик помощь":
                 vk.messages.send(peer_id=event.peer_id, message="---START---\nшизик бот v1.0\ntype: BETA\ncodename: VALDIS\nprefix: шизик\nКоманды:\nшизофазия - бред шизика (слова он черпает от вас!)\nшизофазия вслух - голос шизика! (тоже самое что и шизофазия но в виде голосового сообщения)\nдобавь меня - бот вас добавить (сначала киньте заявку)\nдонат - все деньги идут на хостинг! а еще вас пропустят в рай без очереди ;)\n---END---")
-            elif event.text == "шизик донат":
+            elif event.text.lower() == "шизик донат":
                 vk.messages.send(peer_id=event.peer_id, message="Если у тебя есть желание задонатить то:\nhttp://www.donationalerts.ru/r/feelatkeen")
-            elif "шизик шизофазия: " in event.text: 
+            elif "шизик шизофазия: " in event.text.lower(): 
                 vk.messages.send(peer_id=event.peer_id, message='шизик шизофазия через двоеточие machine broke')
-            elif event.text == "шизик шизофазия помощь":
+            elif event.text.lower() == "шизик шизофазия помощь":
                 vk.messages.send(peer_id=event.peer_id, message="бот набирается слов от вас и несет хуйню")
-            elif "шизик шиз" in event.text:
+            elif "шизик шиз" in event.text.lower():
                 loadwordlist = open("shiza.txt", "r+")
                 wordstring = loadwordlist.read().lower()
                 loadwordlist.close()
@@ -78,7 +78,7 @@ def main():
                         shizasent = shizasent.replace("end", "")
                         shizasent = shizasent.lower()
                         print("shizasent = " + shizasent + ".")
-                        if event.text == "шизик шизофазия":
+                        if event.text.lower() == "шизик шизофазия":
                             vk.messages.send(peer_id=event.peer_id, message=shizasent)
                             try:
                                 global curseconds
@@ -87,7 +87,7 @@ def main():
                             except:
                                 curseconds = curseconds + 24 * 60 * 60
                                 pass
-                        if event.text == "шизик шизофазия вслух":
+                        if event.text.lower() == "шизик шизофазия вслух":
                             shizasent = gTTS(text=shizasent, lang='ru', slow=False)
                             shizasent.save("shiza.mp3")
                             upload = vk_api.VkUpload(vk_session)
@@ -97,7 +97,7 @@ def main():
                             print(sentprikol)
                             vk.messages.send(peer_id=event.peer_id, attachment="doc"+str(sentprikol[0]['owner_id'])+ "_" + str(sentprikol[0]['id']))
                             
-            elif event.text == "шизик выпей амнезиак":
+            elif event.text.lower() == "шизик выпей амнезиак":
                 if event.user_id == "320502491":
                     vk.messages.send(peer_id=event.peer_id, message="*выпил*")
                     loadwordlist = open("shiza.txt", "w")
@@ -106,7 +106,7 @@ def main():
                 else:
                     print(event.user_id)
                     vk.messages.send(peer_id=event.peer_id, message="иди нахуй!!!")
-            elif event.text == "шизик ебани себе по голове":
+            elif event.text.lower() == "шизик ебани себе по голове":
                 if event.user_id == "320502491":
                     vk.messages.send(peer_id=event.peer_id, message="*ебанул*")
                     loadwordlist = open("shiza.txt", "r+")
@@ -122,21 +122,21 @@ def main():
                 else:
                     print(event.user_id)
                     vk.messages.send(peer_id=event.peer_id, message="иди нахуй!!!")
-            elif "шизик забудь:" in event.text:
+            elif "шизик забудь:" in event.text.lower():
                 if event.user_id == "320502491":
                     vk.messages.send(peer_id=event.peer_id, message="что это значит?")
                     loadwordlist = open("shiza.txt", "r+")
                     prikolwordlist = loadwordlist.read()
                     loadwordlist.close()
-                    prikolwordlist = prikolwordlist.replace(event.text.replace("шизик забудь: ", ""), "")
+                    prikolwordlist = prikolwordlist.replace(event.text.lower().replace("шизик забудь: ", ""), "")
                     editwordlist = open("shiza.txt", "w+")
                     editwordlist.write(prikolwordlist)
                     editwordlist.close()
                 else:
                     print(event.user_id)
                     vk.messages.send(peer_id=event.peer_id, message="иди нахуй!!!")
-            elif "шизик убить " in event.text:
-                usercmd = event.text.replace("шизик убить ", "")
+            elif "шизик убить " in event.text.lower():
+                usercmd = event.text.lower().replace("шизик убить ", "")
                 if event.user_id == "320502491":
                     vk.messages.send(peer_id=event.peer_id, message="[id" + usercmd + "|шайтан] убить")
                     banread = open("banned.txt", "r")
@@ -151,8 +151,8 @@ def main():
                     banread.close()
                 else:
                     vk.messages.send(peer_id=event.peer_id, message="иди нахуй!!!")
-            elif "шизик воскреси " in event.text:
-                usercmd = event.text.replace("шизик воскреси ", "")
+            elif "шизик воскреси " in event.text.lower():
+                usercmd = event.text.lower().replace("шизик воскреси ", "")
                 if event.user_id == "320502491":
                     vk.messages.send(peer_id=event.peer_id, message="[id" + usercmd + "|нешайтан] восресить")
                     banread = open("banned.txt", "r")
@@ -173,7 +173,7 @@ def main():
                     print(event.user_id + " ignore!")
                 else:
                     try:
-                        prikoltexta = event.text.split(" ")
+                        prikoltexta = event.text.lower().split(" ")
                         readwordlist = open("shiza.txt", "r")
                         savedtext = readwordlist.read()
                         readwordlist.close()
@@ -182,7 +182,7 @@ def main():
                             savedtext = savedtext.replace("\n", " ")
                             savedtext = savedtext.replace(",", " ")
                             loadwordlist = open("shiza.txt", "w")
-                            loadwordlist.write(savedtext + event.text + " END ")
+                            loadwordlist.write(savedtext + event.text.lower() + " END ")
                             loadwordlist.close()
                         else:
                             vk.messages.send(peer_id=event.peer_id, message="слишком много слов!!!! я не понимаю!!!!!!!!!!!!!!!!")
