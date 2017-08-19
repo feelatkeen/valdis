bee703e4335dd8b4f2f6730ddb41a46cc95fda64
Vlad Filatov <vladfox1337@yandex.ru>
2017-08-19 11:39:40 +0300

    chto

--- valdismarkov.py
+++ valdismarkov.py
@@ -13,12 +13,16 @@ def generate(chain, length):
     """ generate new text given length from the chain """
     current_word = random.choice(list(chain.keys()))
     ret = [current_word]
-    for _ in range(length):
+    sentlen = length
+    for i in range(sentlen):
         next_pairs = chain[current_word].items()
         next_words, weights = list(zip(*next_pairs))
         current_word = random.choices(next_words, weights)[0]
         if current_word not in ret:
             ret.append(current_word)
+        if current_word == "end":
+            sentlen = i
+
     return ' '.join(ret)
 
 #спасибо Zagrebelin за скрипт
